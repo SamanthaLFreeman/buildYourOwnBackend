5455277d6a82905c85a5f78916b6dd9c271aba88
Alan Turing <freemasl.sf@gmail.com>
2019-10-02 15:09:14 -0600

    Setup knexfile and stateSkiResorts database

--- knexfile.js
+++ knexfile.js
@@ -3,9 +3,14 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
+    client: 'pg',
+    connection: 'postgres://localhost/stateSkiResorts',
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds'
     }
   }
 };
