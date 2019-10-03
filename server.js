d0a0d8584c35d9f203e053331eaf673366399af1
Alan Turing <freemasl.sf@gmail.com>
2019-10-03 14:33:23 -0600

    Create GET request for states

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const app = express();
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'skiResortStates';
 app.use(express.json());
 
 app.get('/', (request, response) => {
-  return response.send('Test starting the server');
+  response.send('Find the Ski Resorts for each state!');
+});
+
+app.get('/api/v1/states', (request, response) => {
+  database('states').select()
+    .then((states) => {
+      response.status(200).json(states);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
 });
 
 app.listen(app.get('port'), () => {
